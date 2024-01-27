a4cc3619cd28e952126e8f2bdf168910b5de3362
Rapct <linmeng@kean.edu>
2024-01-28 01:36:42 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿
+using static System.Console;
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, Mengtian Lin");
+Console.WriteLine("Version: {0}", Environment.Version.ToString());
+
+string text = System.IO.File.ReadAllText("text.txt");
+Console.WriteLine(text);
